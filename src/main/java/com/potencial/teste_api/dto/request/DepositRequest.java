22786ded041ae26c043e42e954ffa62d000d24dc
Ighor A. de Oliveira <ighor.oliver1999@gmail.com>
2025-12-12 07:48:41 -0300

    Update DepositRequest.java

--- src/main/java/com/potencial/teste_api/dto/request/DepositRequest.java
+++ src/main/java/com/potencial/teste_api/dto/request/DepositRequest.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 
 //Oq estou pedindo para criar a transaction
 public record DepositRequest(
-        @NotNull(message = "Id da conta é obrigatório!") Long toAccountId,
+        @NotNull(message = "Id da conta é obrigatório!") Long fromAccountId,
         @NotNull @Positive(message = "Valor deve ser maior que 0!") BigDecimal amount
 ) {
 }
